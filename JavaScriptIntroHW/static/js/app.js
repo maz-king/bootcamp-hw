b947ff0cf949a70d58b894702ae2ce2ed49e96b0
Maz <maz.m.king@gmail.com>
2019-11-20 21:07:54 -0600

    almost

--- JavaScriptIntroHW/static/js/app.js
+++ JavaScriptIntroHW/static/js/app.js
@@ -1,43 +1,54 @@
 // from data.js
 var tableData = data;
 
+var tbody = d3.select("tbody");
+
 // function for submit button
 function handleSubmit() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
   
     // Select the input value from the form
-    var userdate = d3.select("#datetime").node().value;
+    var userdate = d3.select("#datetime").property("value");
     console.log(userdate);
 
-    // variable to connect input and table data
-    var search = data.filter(userdate);
-    console.log(search);
+    filterData = tableData.filter((row) => row.datetime === userdate)
   
     // clear the input value
     // d3.select("#datetime").node().value = "";
   
     // Build the table
-    buildTable(table);
+    buildTable(tableData);
 }
 
 
 // function to build table
-function buildTable(date, city, State, Country, shape, duration, comments) {
-    var table = d3.select("#ufo-table");
-    var tbody = table.select("tbody");
-    var trow;
-    for (var i = 0; i < search.length; i++) {
-      trow = tbody.append("tr");
-      trow.append("td").text(datetime[i]);
-      trow.append("td").text(city[i]);
-      trow.append("td").text(state[i]);
-      trow.append("td").text(country[i]);
-      trow.append("td").text(shape[i]);
-      trow.append("td").text(durationMinutes[i]);
-      trow.append("td").text(comments[i]);
-    }
+function buildTable(tableData) {
+    //tbody.text("");
+    data.forEach((row) => {
+        var row = tbody.append("tr");
+        Object.entries(row).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+    });
 }
+      
+
+    // var table = d3.select("#ufo-table");
+    // var tbody = table.select("tbody");
+    // var trow;
+    // for (var i = 0; i < inputvalue.length; i++) {
+    //   trow = tbody.append("tr");
+    //   trow.append("td").text(datetime[i]);
+    //   trow.append("td").text(city[i]);
+    //   trow.append("td").text(state[i]);
+    //   trow.append("td").text(country[i]);
+    //   trow.append("td").text(shape[i]);
+    //   trow.append("td").text(durationMinutes[i]);
+    //   trow.append("td").text(comments[i]);
+    // }
+
   
   
   
